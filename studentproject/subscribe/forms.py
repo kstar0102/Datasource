cdda095f56ddc9837c7503f46be117bc7c24cbfb
Super Dev <66872038+kstar0102@users.noreply.github.com>
2021-04-22 16:38:30 +0900

    Update forms.py

--- studentproject/subscribe/forms.py
+++ studentproject/subscribe/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 class Subscribe(forms.Form):
     Email = forms.EmailField()
+    subject=forms.CharField(max_length=100)
+    message=forms.CharField(max_length=500)
     
     def __str__(self):
         return self.Email
